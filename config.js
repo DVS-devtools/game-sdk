9a567afdabc0fc651e54b7939816e534cb864539
gianni <fiorentinig@gmail.com>
2019-03-25 14:27:46 +0100

    sdk lib

--- config.js
+++ config.js
@@ -3,8 +3,6 @@ exports.generic = {
     "local_domain":"local.www.gameasy.com",
     "domain": "www.gameasy.it",
     "port": "8080",
-    "sdk_vendor":"http://s.motime.com/js/wl/webstore_html5game/gfsdk/3.x.x/dist/vendor.9b94e.min.js?v=20190311110622",
-    "sdk":"http://s.motime.com/js/wl/webstore_html5game/gfsdk/3.x.x/dist/gfsdk.9c3d8.min.js?cache=false&v=20190311110622",
     "newton":"http://static.newton.pm/js/v2.x/newton.min.js"
 }
 
